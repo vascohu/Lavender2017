024a8d50a894db16a9cfda63c922b9b52de04c9e
Vascohu <hzhphoenix@gmail.com>
2017-03-13 14:25:03 +0800

    Debug Version for MV and EM

--- EMLearning.py
+++ EMLearning.py
@@ -2,13 +2,13 @@ import numpy as np
 import scipy.stats as st
 import math as math
 
-np.random.seed(1234)
+#np.random.seed(1234)
 
 
 # Generate the synthetic data : s\in {0,1,2}
 def syn_label_gene(task_num, worker_num, label_num):
     __true_labels = st.bernoulli(0.5, loc=1).rvs(size=task_num)
-    worker_quality = st.uniform(loc=0.5, scale=0.5).rvs(size=worker_num)
+    worker_quality = st.uniform(loc=0.4, scale=0.5).rvs(size=worker_num)
     sample_no = np.random.choice(task_num*worker_num, label_num, replace=False)
     __label_mat = np.zeros([task_num, worker_num])
     for i in sample_no:
@@ -52,23 +52,21 @@ def em_filter(__label_mat, __label_num=2):
     __label_tensor = np.zeros([__worker_num, __label_num, __label_num], dtype=float)
 
     # The labels is initialized as the results of majority voting
-    __init_labels = maj_voting(__label_mat, __label_num)
+    __init_task_label_prob = maj_voting(__label_mat, __label_num)
 
     # Initialize the label tensor
+    for i in range(__task_num):
+        for j in range(__worker_num):
+            __ob_label_ij = __label_mat[i, j]
+            if __ob_label_ij > 0:
+                __label_tensor[j, :, __ob_label_ij - 1] += __init_task_label_prob[i, :]
     for i in range(__worker_num):
-        __worker_labels = __label_mat[:, i]
-
-        for j in range(__task_num):
-            __real_label = __init_labels[j]
-            __observed_label = __worker_labels[j]
-            __label_tensor[i, __real_label-1, __observed_label-1] += 1
-
         for j in range(__label_num):
-            __label_sum = np.sum(__label_tensor[i, j, :])
-            if __label_sum == 0:
-                __label_tensor[i, j, :] = np.ones(__label_num, dtype=float) * 1.0 / __label_num
+            __prob_sum = np.sum(__label_tensor[i, j, :])
+            if __prob_sum != 0:
+                __label_tensor[i, j, :] /= __prob_sum
             else:
-                __label_tensor[i, j, :] /= __label_sum
+                __label_tensor[i, j, :].fill(1.0 / __label_num)
 
     # E-M iteration to filter the true labels
     __label_tensor0 = np.zeros([__worker_num, __label_num, __label_num], dtype=float)
@@ -80,8 +78,9 @@ def em_filter(__label_mat, __label_num=2):
         for i in range(__task_num):
             for j in range(__worker_num):
                 __ob_label_ij = __label_mat[i, j]
-                __prob_inc_vec = np.log(__label_tensor[j, :, __ob_label_ij-1])
-                __label_dist[i, :] += __prob_inc_vec
+                if __ob_label_ij > 0:
+                    __prob_inc_vec = np.log(__label_tensor[j, :, __ob_label_ij-1])
+                    __label_dist[i, :] += __prob_inc_vec
         __task_label_prob = np.exp(__label_dist)
         for i in range(__task_num):
             __task_label_prob[i, :] /= np.sum(__task_label_prob[i, :])
@@ -91,7 +90,8 @@ def em_filter(__label_mat, __label_num=2):
         for i in range(__task_num):
             for j in range(__worker_num):
                 __ob_label_ij = __label_mat[i, j]
-                __label_tensor[j, :, __ob_label_ij-1] += __task_label_prob[i, :]
+                if __ob_label_ij > 0:
+                    __label_tensor[j, :, __ob_label_ij-1] += __task_label_prob[i, :]
         for i in range(__worker_num):
             for j in range(__label_num):
                 __prob_sum = np.sum(__label_tensor[i, j, :])
@@ -116,10 +116,15 @@ def em_filter(__label_mat, __label_num=2):
 
 
 if __name__ == "__main__":
-    (true_labels, label_mat) = syn_label_gene(10, 10, 10)
-    print(true_labels)
-    print(label_mat)
+    (true_labels, label_mat) = syn_label_gene(10, 10, 100)
+    # print(true_labels)
+    # print(label_mat)
+    # online_labels = np.zeros(label_mat.shape, dtype=int)
+    # online_labels[0: 2, 0: 5] = label_mat[0: 2, 0: 5]
     task_label_prob = maj_voting(label_mat)
-    #label_tensor = em_filter(label_mat)
-    print(task_label_prob)
-    #print(label_tensor)
+    (label_tensor, task_label_prob_em) = em_filter(label_mat)
+    # print(online_labels)
+    # print(task_label_prob)
+    # print(task_label_prob_em)
+    for i in range(10):
+        print(true_labels[i], '\t', task_label_prob[i, :], '\t', task_label_prob_em[i, :])
